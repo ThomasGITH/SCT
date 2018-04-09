cf0fe9edaea7ad13e80fb67f062f22a7a4f053ae
ThomasGITH <31830553+ThomasGITH@users.noreply.github.com>
2018-04-09 13:07:05 +0200

    Add files via upload

---
+++ rekenspel 2.0/script.js
@@ -0,0 +1,49 @@
+const wrapper = document.getElementById('wrapper');
+const myAssignment = document.getElementById('myAssignment');
+const myAnswer = document.getElementById('myAnswer');
+const feedback = document.getElementById('feedback');
+
+let mySum;
+let sound = new Audio();
+
+var row1 = [];
+
+
+function makeSum(){
+  console.log("makeSum called");
+  var a = Math.floor(Math.random() * 9 + 1);
+  var b = Math.floor(Math.random() * 9 + 1);
+  mySum = a + " * " + b;
+  row1[0] = a;
+  row1[1] = b;
+  myAssignement.innerHTML = mySum;
+  myAnswer.focus();
+}
+
+function keyPressed(event){
+  var key = event.which || event.keyCode;
+  if(key == 13){
+    if(eval(mySum) == myAnswer.value){
+      feedback.src = "img/good.png";
+      sound.src = "img/right.wav";
+      sound.play();
+      console.log("Right answer");
+    }else{
+      feedback.src = "img/wrong.png";
+      sound.src = "img/wrong.mp3";
+      sound.play();
+      console.log("Wrong answer");
+    }
+    window.setTimeout(waiting, 2000)
+    console.log(row1);
+    makeSum();
+  }
+}
+
+function waiting(){
+  console.log("waiting called");
+  feedback.src = "img/blank.png";
+  myAnswer.value = "";
+}
+
+makeSum();
